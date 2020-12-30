c275a03d3f76a84e81f042ce32d8b6124c3cc6af
maylacdao <may.lacdao@gmail.com>
2020-12-29 21:48:17 -0500

    Create scrape_mars.py

---
+++ scrape_mars.py
@@ -0,0 +1,13 @@
+def scrape():
+
+    # Import dependencies:
+    import pandas as pd
+    import json
+    import requests
+    from bs4 import BeautifulSoup as bs
+    import splinter
+    from splinter import Browser
+    from splinter.exceptions import ElementDoesNotExist
+    import time
+
+    return Mars_Info
